f8b74a28e9ed1a80460513617f957548e9fbcea1
Jose Wendel <wendelpviana@gmail.com>
2025-05-16 18:39:53 -0300

    :feat: added document request

--- controllers/ControllerClients.js
+++ controllers/ControllerClients.js
@@ -5,11 +5,10 @@ class Clients {
     async getClients(req, res) {
         try {
             const nameUrl = req.body.nome.replace(/\s+/g, "%20")
+            const cpf = req.body.doc.replace(/[.-]/g, '')
 
-            const data = await instance.get(`/apps/api/terceiros/?inicio=0&quantidade=10&nome=${nameUrl}&token=${process.env.API_AUTHORIZATION_CODE}`)
-            const clients = data.data
-            .filter((type) => type.tipoPessoa == "PF")
-            .map((client) => {
+            const data = await instance.get(`/apps/api/terceiros/?inicio=0&quantidade=10&tipopessoa=pf&nome=${nameUrl}&documento=${cpf}&token=${process.env.API_AUTHORIZATION_CODE}`)
+            const clients = data.data.map((client) => {
                 return {
                     name: client.nome,
                     document: client.documento == "" ? "Sem documento" : client.documento,
