21426ed624d8b7093973e54eb546aa9c70266b14
Jose Wendel <wendelpviana@gmail.com>
2025-05-19 08:24:36 -0300

    :fix: bug fixed

--- controllers/ControllerClients.js
+++ controllers/ControllerClients.js
@@ -4,11 +4,28 @@ require("dotenv").config()
 class Clients {
     async getClients(req, res) {
         try {
-            const nameUrl = req.body.nome.replace(/\s+/g, "%20")
-            const cpf = req.body.doc.replace(/[.-]/g, '')
+            const {nome, doc} = req.body
 
-            const data = await instance.get(`/apps/api/terceiros/?inicio=0&quantidade=10&tipopessoa=pf&nome=${nameUrl}&documento=${cpf}&token=${process.env.API_AUTHORIZATION_CODE}`)
-            const clients = data.data.map((client) => {
+            if(nome === "" && doc === "") {
+                return res.json({messages: {
+                    alert: "Nenhum dado disponível",
+                    message: "Preencha os campos acima para realizar a busca por um cliente."
+                }})
+            }
+
+            const nameUrl = nome.replace(/\s+/g, "%20")
+            const cpf = doc.replace(/[.-]/g, '')
+
+            const response = await instance.get(`/apps/api/terceiros/?inicio=0&quantidade=10&tipopessoa=pf&nome=${nameUrl}&documento=${cpf}&token=${process.env.API_AUTHORIZATION_CODE}`)
+            
+            if(Array.isArray(response.data) && response.data.length === 0) {
+                return res.json({messages: {
+                    alert: "Cliente não encontrado",
+                    message: "Por favor, verifique se os critérios de busca foram preenchidos corretamente."
+                }})
+            }
+            
+            const clients = response.data.map((client) => {
                 return {
                     name: client.nome,
                     document: client.documento == "" ? "Sem documento" : client.documento,
@@ -17,6 +34,7 @@ class Clients {
                     contact: client.telefones[0] == undefined ? "Sem contato" : `(${client.telefones[0].ddd}) ${client.telefones[0].numero}`,
                 }
             })
+
             res.json(clients)
         } catch (error) {
             console.log(error)
